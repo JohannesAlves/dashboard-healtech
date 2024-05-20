ad7731702bb9db29edfad0dbccf46b44863c1997
johannesalves <johannes001002@gmail.com>
2024-05-20 16:41:03 -0300

    chore: tests to login template page

--- src/templates/Login/index.test.tsx
+++ src/templates/Login/index.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  screen,
+  waitFor,
+  getByText,
+  getAllByText,
+} from '@testing-library/react';
 import Login from '.';
-import { AuthProvider } from '@/context/auth-context';
+import { AuthProvider, AuthContext } from '@/context/auth-context';
 
-describe('Login component', () => {
+describe('<Login />', () => {
   it('renders login form correctly', () => {
     const { getByLabelText, getByText } = render(<Login />);
 
@@ -16,5 +23,92 @@ describe('Login component', () => {
     expect(getByLabelText('Senha')).toBeInTheDocument();
     expect(getByText('Esqueceu a sua senha?')).toBeInTheDocument();
     expect(getByText('Acessar plataforma')).toBeInTheDocument();
+  });
+
+  it('should be call handleLogin when submit is fired', async () => {
+    const mockHandleLogin = jest.fn();
+    mockHandleLogin.mockReturnValue({ isSigned: false });
+
+    const { getByRole, getByLabelText, getByText } = render(
+      <AuthContext.Provider
+        value={{ handleLogin: mockHandleLogin, user: { isSigned: false } }}
+      >
+        <Login />
+      </AuthContext.Provider>
+    );
+
+    fireEvent.change(getByLabelText(/email/i), {
+      target: { value: 'teste@example.com' },
+    });
+    fireEvent.change(getByLabelText(/senha/i), {
+      target: { value: 'password' },
+    });
+
+    await waitFor(() => {
+      fireEvent.click(getByRole('button', { name: /acessar plataforma/i }));
+      expect(mockHandleLogin).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should be return error messsage when email and password is wrong', async () => {
+    const mockHandleLogin = jest.fn();
+    mockHandleLogin.mockReturnValue({ isSigned: false });
+    const mockUser = { isSigned: false };
+
+    const { getByRole, getByLabelText, getByText } = render(
+      <AuthContext.Provider
+        value={{ handleLogin: mockHandleLogin, user: mockUser }}
+      >
+        <Login />
+      </AuthContext.Provider>
+    );
+
+    fireEvent.change(getByLabelText(/email/i), {
+      target: { value: 'wrong@example.com' },
+    });
+    fireEvent.change(getByLabelText(/senha/i), {
+      target: { value: 'wrongpassword' },
+    });
+
+    await waitFor(() => {
+      fireEvent.click(getByRole('button', { name: /acessar plataforma/i }));
+      const errorsMessage = screen.getAllByText('Email ou senha incorretos.');
+
+      expect(errorsMessage.length).toBe(2);
+    });
+  });
+
+  it('should be return signed true when user make the login correctly', async () => {
+    const mockHandleLogin = jest.fn();
+    mockHandleLogin.mockReturnValue({
+      isSigned: true,
+      email: 'teste@gmail.com',
+      token: 'uuid',
+    });
+    const mockUser = {
+      isSigned: true,
+      email: 'teste@gmail.com',
+      password: '1234',
+    };
+
+    const { getByRole, getByLabelText, getByText } = render(
+      <AuthContext.Provider
+        value={{ handleLogin: mockHandleLogin, user: mockUser }}
+      >
+        <Login />
+      </AuthContext.Provider>
+    );
+
+    fireEvent.change(getByLabelText(/email/i), {
+      target: { value: 'teste@gmail.com' },
+    });
+    fireEvent.change(getByLabelText(/senha/i), {
+      target: { value: '1234' },
+    });
+
+    await waitFor(() => {
+      fireEvent.click(getByRole('button', { name: /acessar plataforma/i }));
+      expect(mockUser.isSigned).toBe(true);
+    });
   });
 });
