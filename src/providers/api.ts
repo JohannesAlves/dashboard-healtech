6b5076804cce9b297ee79d44be777f90914ba3b9
johannesalves <johannes001002@gmail.com>
2024-05-22 13:28:44 -0300

    deploy: add backend url on base url

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:4000';
+const baseURL = 'http://localhost:4000' || process.env.BACKEND_URL;
 
 const jsonServerApi = axios.create({
   baseURL,
